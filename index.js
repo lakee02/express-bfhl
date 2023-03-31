dddd75fb859d5359a1d268fa7a67e3ff809d9e60
lakee02 <103621550+lakee02@users.noreply.github.com>
2023-03-31 11:51:32 +0530

    1 file modified

--- index.js
+++ index.js
@@ -12,14 +12,31 @@ app.get('/', (req, res) => {
 });
 
 app.post('/bfhl', (req, res) => {
+  const e=[];
+  for (var i = 0; i < req.body.data.length; ++i) {
+    if ((req.body.data[i] % 2) == 0) {
+        e.push(req.body.data[i]);
+  }
+  const o=[];
+  for (var i = 0; i < req.body.data.length; ++i) {
+    if ((req.body.data[i] % 3) != 0) {
+        o.push(req.body.data[i]);
+  }
+  const cArray=[]
+  for (var i = 0; i < req.body.data.length; ++i) {
+        req.body.data[i].toUpperCase() 
+        c.push(req.body.data[i].toUpperCase() );
+  }
+  string.toUpperCase()
+}
   const data = {
     is_success: 'true',
     user_id: 'john_doe_17091999',
     email: 'john@xyz.com',
     roll_number: 'ABCD123',
-    odd_numbers: ['5'],
-    even_numbers: [2, 4, 92],
-    alphabets: 'B',
+    odd_numbers: o,
+    even_numbers: e,
+    alphabets: cArray,
   };
 
   res.send(data);
